37ac40c8f29181e09f5917308cc36deb47878063
Amarto <amarto.rajaram@gmail.com>
2014-06-06 13:35:19 -0400

    Created separate methods to prompt user response. Created method isLegal

--- Game.java
+++ Game.java
@@ -13,8 +13,7 @@ public class Game
 	private Player computerPlayer;
 	private TicTacToeBoard board;
 	private boolean isWinner;
-	//describes a Game with a board and two players
-	//judges whether a move is legal
+	Scanner in;
 	
 	public Game()
 	{
@@ -22,6 +21,7 @@ public class Game
 		computerPlayer = new Player();
 		board = new TicTacToeBoard();
 		isWinner = false;
+		in = new Scanner(System.in);
 		
 	}
 	
@@ -35,17 +35,19 @@ public class Game
 			
 			if (random == 0)
 			{
-				humanPlayer.getBoard(board);
-				board.printBoard();
-				System.out.println("Please enter the row index: ");
-				int row = in.nextInt();
-				System.out.println("Please enter the column index: ");
-				int column = in.nextInt();
-				System.out.println("Please enter the character (x or o): ");
-				String s = in.nextLine();
-				char ch = s.charAt(0);
+				int row = promptRow();
+				int column = promptColumn();
+				char ch = promptChar();
+				
+				while (!isLegal(row, column))
+				{	
+					row = promptRow();
+					column = promptColumn();
+					ch = promptChar();
+				}
+				
 				humanPlayer.move(row, column, ch);
-				board = humanPlayer.updateBoard();
+				board = humanPlayer.updateBoard();				
 			}
 			
 			if (random == 1)
@@ -66,6 +68,52 @@ public class Game
 			
 		}
 		
+	}
+	
+	public boolean checkWinner()
+	{
+		
+		return isWinner;
+	}
+	
+	public boolean isLegal(int row, int column)
+	{
+		boolean isLegal = true;
+		
+		if (board.getValue(row, column) != ' ')
+		{
+			isLegal = false;
+		}
+		
+		return isLegal;
+		
+	}
+	
+	public int promptRow()
+	{
+		System.out.println("Please enter the row index: ");
+		int row = in.nextInt();
+		
+		return row;
+		
+		
+		
+	}
+	
+	public int promptColumn()
+	{
+		System.out.println("Please enter the column index: ");
+		int column = in.nextInt();
+		
+		return column;
+	}
+	
+	public char promptChar()
+	{
+		System.out.println("Please enter the character (x or o): ");
+		String s = in.nextLine();
+		char ch = s.charAt(0);
+		return ch;
 	}
 	
 
